756a3616704009a15408c3d3ea113fe4ce82a132
KritantaDev <KritantaDevelopment@gmail.com>
2020-05-11 18:53:15 -0500

    V1 Changes

--- DragonGen/DragonGen.py
+++ DragonGen/DragonGen.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
-import enum
-import string
+import os
+import re
+import subprocess
+import sys
 import traceback
 from datetime import datetime
-import subprocess
-import re
-import os
-import sys
-from time import sleep
-from typing import Dict, Any, List, Union, TextIO
-from DragonExceptions import *
-from genbuild.genbuild.generator import *
-import yaml
-import pprint
+from typing import List, TextIO
 
-# from DragonGen.self.builder_syntax import Writer
 import yaml
+
+from DragonExceptions import *
+from buildgen.buildgen.generator import Generator
 
 exports = {}
-
+# store current variables here in the event of exception
+variables_dump = {}
 # Legacy regex to match variables in makefiles and dragon bash configs
 dragon_match = '(.*)="?(.*)""?#?'
 make_match = '(.*)=(.*)#?'
@@ -35,9 +31,6 @@ class Project(object):
         'signdir': '$pdirname/sign',
         'bridging-header': '$name-Bridging-Header.h',
         'dragondir': '$$DRAGONBUILD',
-        'targetios': '10.0',
-        'archs': ['armv7', 'arm64', 'arm64e'],
-        'sysroot': '$dragondir/sdks/iPhoneOS.sdk',
         'cc': 'clang',
         'cxx': 'clang++',
         'ld': 'clang++',
@@ -48,8 +41,6 @@ class Project(object):
         'logos': '$dragondir/bin/logos.pl',
         'optool': '$dragondir/bin/optool',
         'stage': ['true'],
-        'arc': True,
-        'targ': '-DTARGET_IPHONE=1',
         'warnings': '-Wall',
         'optim': '0',
         'debug': '-fcolor-diagnostics',
@@ -70,9 +61,10 @@ class Project(object):
     def __init__(self, project_type: str, variables: dict, out: TextIO):
         self.variables = variables
         self.type = project_type
-        self.type_config = yaml.safe_load(open(os.environ['DRAGONBUILD'] + "/DragonGen/ProjectConfiguration.yml"))
+        self.base_configurations = yaml.safe_load(open(os.environ['DRAGONBUILD'] + "/DragonGen/ProjectConfiguration.yml"))
 
         self.builder = Generator(out)
+        self.target = 'ios'
 
         if not self.ignores_state('has_build_files'):
             self.files = get_args(self.variables, 'files')
@@ -102,13 +94,13 @@ class Project(object):
 
     def requires_state(self, state: str):
         try:
-            return state in self.type_config['Types'][self.type]['required_states']
+            return state in self.base_configurations['Types'][self.type]['required_states']
         except (KeyError, TypeError):
             return False
 
     def ignores_state(self, state: str):
         try:
-            return state in self.type_config['Types'][self.type]['ignored_states']
+            return state in self.base_configurations['Types'][self.type]['ignored_states']
         except (KeyError, TypeError):
             return False
 
@@ -126,7 +118,7 @@ class Project(object):
             self.builder.build('stage', 'stage', 'build.ninja')
             return []
         outputs = []
-        targets = ['$target']
+        targets = ['$build_target_file']
         files = get_args(self.variables, 'files')
         swift_files = get_args(self.variables, 'swift_files')
         c_files = get_args(self.variables, 'c_files')
@@ -234,11 +226,10 @@ class Project(object):
         self.builder.build('$signtarget', 'debug', '$symtarget')
         self.builder.newline()
 
-        self.builder.build('$target', 'sign', '$signtarget')
+        self.builder.build('$build_target_file', 'sign', '$signtarget')
         self.builder.newline()
 
         self.builder.build('stage', 'stage', 'build.ninja')
-        #targets.append('stage')
 
         return targets
 
@@ -266,18 +257,17 @@ class Project(object):
 
         self.builder.variable('location', get_var(self.variables, 'install_location'))
         self.builder.variable('resource_dir', get_var(self.variables, 'resource_dir'))
-        self.builder.variable('target', get_var(self.variables, 'target'))
+        self.builder.variable('build_target_file', get_var(self.variables, 'build_target_file'))
         self.builder.newline()
-        #pprint.pprint(self.type_config['Types'][self.type], stream=sys.stderr)
-        self.builder.variable('stage2', self.type_config['Types'][self.type]['variables']['stage2'])
+        self.builder.variable('stage2', extrapolate_stage(self.base_configurations['Types'][self.type]['variables']['stage2']))
         self.builder.newline()
 
         self.builder.variable('stagedir', get_var(self.variables, 'stagedir'))
         self.builder.variable('builddir', get_var(self.variables, 'builddir'))
         self.builder.variable('objdir', get_var(self.variables, 'objdir'))
         self.builder.variable('signdir', get_var(self.variables, 'signdir'))
-        self.builder.variable('signtarget', '$signdir/$target.unsigned')
-        self.builder.variable('symtarget', '$signdir/$target.unsym')
+        self.builder.variable('signtarget', '$signdir/$build_target_file.unsigned')
+        self.builder.variable('symtarget', '$signdir/$build_target_file.unsym')
         self.builder.newline()
 
         self.builder.variable('dragondir', get_var(self.variables, 'dragondir'))
@@ -305,7 +295,7 @@ class Project(object):
         self.builder.variable('stage', extrapolate_stage(self.variables['stage']))
         self.builder.newline()
 
-        self.builder.variable('targetios', get_var(self.variables, 'targetios'))
+        self.builder.variable('targetvers', get_var(self.variables, 'targetvers'))
         self.builder.newline()
         self.builder.variable('frameworks', get_var(self.variables, 'frameworks'))
 
@@ -314,7 +304,7 @@ class Project(object):
         self.builder.variable('libs', get_var(self.variables, 'libs'))
         self.builder.newline()
 
-        self.builder.variable('arc', '-fobjc-arc' if get_var(self.variables, 'arc') else '')
+        self.builder.variable('arc', '-fobjc-arc' if self.variables['arc'] else '')
         self.builder.variable('btarg', get_var(self.variables, 'targ'))
         self.builder.variable('warnings', get_var(self.variables, 'warnings'))
         self.builder.variable('optim', '-O' + get_var(self.variables, 'optim'))
@@ -336,8 +326,8 @@ class Project(object):
 
         cflags = '$cinclude -fmodules -fcxx-modules -fmodule-name=$name -fbuild-session-file=$pdirname/modules/ ' \
                  '-fmodules-prune-after=345600 -fmodules-prune-interval=86400 -fmodules-validate-once-per-build-session ' \
-                 '$arc $fwSearch -miphoneos-version-min=$targetios -isysroot $sysroot $btarg $warnings $optim $debug ' \
-                 '$usrCflags $header_includes '
+                 '$arc $fwSearch -miphoneos-version-min=$targetvers -isysroot $sysroot $btarg $warnings $optim $debug ' \
+                 '$usrCflags $header_includes'
         self.builder.variable('cflags', cflags)
         self.builder.newline()
 
@@ -478,7 +468,7 @@ class Project(object):
                           command="$dsym \"$in\" 2&> /dev/null; cp $in $out")
         self.builder.newline()
         self.builder.rule('sign', description="Signing $name",
-                          command="$codesign $idflag $entflag$entfile $in && cp $in $out")
+                          command="$codesign -S $in && cp $in $out")
         self.builder.newline()
         self.builder.rule('otool', description="Injecting $name",
                           command="true;")
@@ -486,19 +476,42 @@ class Project(object):
         self.builder.rule('stage', description="Running Stage for $name", command="$stage; $stage2")
         self.builder.newline()
 
+    # noinspection PyTypeChecker
     def create_variable_dict(self):
         """
 
         """
         variables = Project.default_variables.copy()
 
-        variables.update(self.type_config['Types'][self.type]['variables'])
+        if not self.target:
+            self.target = 'ios'
+
+        variables.update(self.base_configurations['Targets'][self.target])
+
+        variables.update(self.base_configurations['Types'][self.type]['variables'])
+
+        if 'all' in variables:
+            variables.update(variables['all'])
+
+        if 'targets' in variables:
+            if self.target in variables['targets']:
+                if 'all' in variables['targets'][self.target]:
+                    variables.update(variables['targets'][self.target]['all'])
+                if self.variables['name']:
+                    variables.update(variables['targets'][self.target][variables['name']])
+
+        if self.target in ['ios']:
+            print("export DRAGON_DPKG=1")
+
 
         variables.update(self.variables)
 
         self.variables = variables.copy()
-        # import pprint
-        # pprint.pprint(f'\n\n{self.variables["name"]}:\n\n' + str(self.variables), stream=sys.stderr)
+        global variables_dump
+        variables_dump = self.variables.copy()
+        if os.environ['DGEN_DEBUG']:
+            import pprint
+            pprint.pprint(f'\n\n{self.variables["name"]}:\n\n' + str(self.variables), stream=sys.stderr)
 
 
 crucial_variables = ['name', 'type', 'dir', 'cc', 'cxx', 'ld', 'codesign']
@@ -558,7 +571,7 @@ def get_var(full_vars, name, is_empty=None):
         else:
             raise ex
     if name in argument_variables.keys():
-        value = arg_list(value) if name not in ['stage'] else extrapolate_stage(value)
+        value = arg_list(value) if name not in ['stage', 'stage2'] else extrapolate_stage(value)
         if name in supports_expressions:
             lis = value.split(' ')  # hotfix
             for i in lis:
@@ -635,7 +648,7 @@ def main():
         elif i == 'exports':
             exports.update(config[i])
             continue
-        elif i in ['id', 'author', 'version', 'depends', 'packaging', 'desc']:
+        elif i in ['id', 'author', 'version', 'depends', 'package', 'desc']:
             continue
 
         project_config = {
@@ -643,7 +656,8 @@ def main():
             'dir': '.'
         }
         project_config.update(config[i])
-        # print("Config:" + str(project_config), file=sys.stderr)
+        if os.environ['DGEN_DEBUG']:
+            print("Config:" + str(project_config), file=sys.stderr)
         project_variables = project_config.copy()
         f = open(f"{project_config['dir']}/build.ninja", 'w+')
         proj = Project(project_config['type'], project_variables, f)
@@ -683,8 +697,22 @@ if __name__ == "__main__":
         print("exit 5")
     except KeyError as ex:
         print("KeyError: Something was not found, but it should have been...", file=sys.stderr)
-        print(''.join(traceback.format_tb(ex.__traceback__)), file=sys.stderr)
+        # print(''.join(traceback.format_tb(ex.__traceback__)), file=sys.stderr)
         print(str(ex), file=sys.stderr)
+        print("Press v for detailed debugging output, any other key to exit.", file=sys.stderr)
+
+        import sys, tty, termios
+
+        old_setting = termios.tcgetattr(sys.stdin.fileno())
+        tty.setraw(sys.stdin)
+        x = sys.stdin.read(1)
+        if str(x).lower() == 'v':
+            termios.tcsetattr(0, termios.TCSADRAIN, old_setting)
+            import pprint
+            print("Entire Project Config:", file=sys.stderr)
+            pprint.pprint(variables_dump, stream=sys.stderr)
+            print(''.join(traceback.format_tb(ex.__traceback__)), file=sys.stderr)
+            print(str(ex), file=sys.stderr)
         print("exit 2")
     except Exception as ex:
         import sys, tty, termios
