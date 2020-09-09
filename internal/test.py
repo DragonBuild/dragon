6f261d733867556b8786cda057c774ba698f9486
KritantaDev <KritantaDevelopment@gmail.com>
2020-09-09 17:21:15 -0500

    [Dragon] Test system improvements

--- internal/test.py
+++ internal/test.py
@@ -1,27 +1,10 @@
 import subprocess, sys, os
 from pprint import pprint
 from math import sin, cos, radians
-import timeit, time
+import timeit, time, yaml
 
-cpu_bench = 0
-
-projects = \
-    {
-        'Common':
-            [
-                'Chapters',
-                'Docky',
-                'Shakelight'
-            ],
-        'Legacy':
-            [
-                'StatusViz',
-            ],
-        'Theos':
-            [
-                'StopCrashingPls'
-            ]
-    }
+TestDict = yaml.safe_load(open(os.environ['DRAGONBUILD'] + '/internal/tests.yml'))
+projects = TestDict['ProjectTests']
 
 
 def bench():
@@ -33,28 +16,9 @@ def bench():
     return product
 
 
-def perform_benchmark():
-    print("Doing CPU Benchmark:")
-    result = timeit.repeat('test.bench()', setup='import test', number=10, repeat=5)
-    result = list(sorted(result))
-    global cpu_bench
-    cpu_bench = sum(result[:3]) / 3
-    print(cpu_bench)
-    print("Doing Project Build Benchmark")
-    benchmark_projects()
-
-
-def benchmark_projects():
-    #os.chdir(os.environ['DRAGONBUILD'] + '/testing/' + projects['Common'][0])
-    #result = timeit.timeit('test.system("dragon c b")', setup='import test, sys', number=10)
-    #print(result / 10)
-    # result = list(sorted(result))
-    # print(*result[:3])
-    pass
-
 
 def system(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE):
-    proc = subprocess.Popen(cmd,
+    proc = subprocess.Popen("" + cmd,
                             stdout=stdout,
                             stderr=stderr,
                             shell=True,
@@ -71,11 +35,13 @@ def main():
     times = {
 
     }
+    if len(os.environ['DRAGONBUILD']) > 0:
+        system('rm -rf ' + os.environ['DRAGONBUILD'] + '/testing/')
     system('mkdir -p ' + os.environ['DRAGONBUILD'] + '/testing/')
     os.chdir(os.environ['DRAGONBUILD'] + '/testing/')
 
-    print('Doing Benchmarks')
-    perform_benchmark()
+    # print('Doing Benchmarks')
+    # perform_benchmark()
 
     for category in projects:
         cattests = {}
@@ -85,20 +51,23 @@ def main():
             print(os.getcwd())
             system('''
             echo $PWD
-            git clone https://github.com/KritantaDev/%s.git
+            git clone %s
             ''' % i, sys.stdout, sys.stderr)
-            os.chdir(i)
+            dirName = i.split('/')[-1]
+            os.chdir(dirName)
             print(os.getcwd())
-            print(f"---\n Testing {i} \n---")
+            print(f"---\n Testing {dirName} \n---")
             # time.sleep(5)
             s = timeit.default_timer()
             passed = system('dragon c b', sys.stdout, sys.stderr) == 0
             s = timeit.default_timer() - s
             print(s)
             print('-+- Passed -+-' if passed else '-!- Failed -!-')
-            cattests[i] = passed
-            cattimes[i] = s
+            cattests[dirName] = passed
+            cattimes[dirName] = s
             os.chdir(os.environ['DRAGONBUILD'] + '/testing/')
+            if passed:
+                system(f'rm -rf {dirName}', sys.stdout, sys.stderr)
         tests[category] = cattests
         times[category] = cattimes
 
@@ -108,28 +77,12 @@ def main():
 
     total = 0
     passed = 0
-    print(times)
-    # print(tests)
-    print(cpu_bench)
-    baseline = {
-        'Common': {
-            'Chapters': 0.7295811089999997/1.3837896153333336,
-            'Docky': 1.3256995099999997/1.3837896153333336,
-            'Shakelight': 0.7603968410000022/1.3837896153333336
-            },
-        'Legacy': {
-            'StatusViz': 1.176904830999998/1.3837896153333336
-            },
-        'Theos': {
-            'StopCrashingPls': 0.631509568000002/1.3837896153333336
-            },
 
-        }
     for category in tests:
         print(f'\n{category} Project Build Tests')
         for i in tests[category]:
             print(
-                f'{i} : {"Passed" if tests[category][i] else "Failed"} in {times[category][i]} : {str(( times[category][i] / cpu_bench ) / baseline[category][i] * 100)[:5]}%')
+                f'{i} : {"Passed" if tests[category][i] else "Failed"} in {times[category][i]} ')
         passed += sum(tests[category].values())
         total += len(tests[category])
 
