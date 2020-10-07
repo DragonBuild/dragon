65b2862533d99ac6755cd06b203060f7ee999fb5
iCrazeiOS <39101269+iCrazeiOS@users.noreply.github.com>
2020-10-07 21:46:09 +0100

    added/updated some popular libraries
    added:
    libimagepicker
    libsparkcolourpicker
    
    updated:
    libcolorpicker

---
+++ include/libimagepicker.h
@@ -0,0 +1,10 @@
+#import  <UIKit/UIKit.h>
+
+#ifdef __cplusplus /* If this is a C++ compiler, use C linkage */
+extern "C" {
+#endif
+UIImage *LIPParseImage(NSData *imageDataFromPrefs);
+
+#ifdef __cplusplus /* If this is a C++ compiler, end C linkage */
+}
+#endif
