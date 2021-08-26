58cd66171386e000f7305835bc7cb4f3acc81642
KritantaDev <KritantaDevelopment@gmail.com>
2021-08-26 13:41:34 -0400

    1.5.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dragon',
-      version='1.5',
+      version='1.5.1',
       description='A powerful toolkit targeting Apple development research, '
       'and packaging.',
       author='kritanta',
