14f020849bfff3774bc81efb41a8c1f45ff1d5b3
KritantaDev <KritantaDevelopment@gmail.com>
2021-08-26 12:46:50 -0400

    Fix #64

--- src/dragongen/util.py
+++ src/dragongen/util.py
@@ -98,7 +98,8 @@ def interpret_theos_makefile(file: object, root: object = True) -> dict:
                 name, value = nepmatch.match(line).group(1, 2)
             if name.strip() in variables:
                 variables[name.strip()] = variables[name.strip()] + ' ' + value.strip()
-            variables[name.strip()] = value.strip()
+            else:
+                variables[name.strip()] = value.strip()
     finally:
         file.close()
 
@@ -121,77 +122,78 @@ def interpret_theos_makefile(file: object, root: object = True) -> dict:
     module_type_naming = module_type.upper()
 
     module_name = variables.get(f'{module_type_naming}_NAME')
-    module_archs = variables.get(f'ARCHS')
-    module_files = variables.get(module_name + '_FILES') or variables.get(f'$({module_type_naming}_NAME)_FILES') or ''
-    module_cflags = variables.get(module_name + '_CFLAGS') or variables.get('$({module_type_naming}_NAME)_CFLAGS') or ''
-    module_cxxflags = variables.get(module_name + '_CXXFLAGS') or variables.get('$({module_type_naming}_NAME)_CXXFLAGS') or ''
-    module_cflags = variables.get(f'ADDITIONAL_CFLAGS') or ''
-    module_ldflags = variables.get(module_name + '_LDFLAGS') or variables.get(
-        f'$({module_type_naming}_NAME)_LDFLAGS') or ''
-    module_codesign_flags = variables.get(module_name + '_CODESIGN_FLAGS') or variables.get(
-        f'$({module_type_naming}_NAME)_CODESIGN_FLAGS') or ''
-    module_ipath = variables.get(module_name + '_INSTALL_PATH') or variables.get(
-        f'$({module_type_naming}_NAME)_INSTALL_PATH') or ''
-    module_frameworks = variables.get(module_name + '_FRAMEWORKS') or variables.get(
-        f'$({module_type_naming}_NAME)_FRAMEWORKS') or ''
-    module_pframeworks = variables.get(module_name + '_PRIVATE_FRAMEWORKS') or variables.get(
-        f'$({module_type_naming}_NAME)_PRIVATE_FRAMEWORKS') or ''
-    module_eframeworks = variables.get(module_name + '_EXTRA_FRAMEWORKS') or variables.get(
-        f'$({module_type_naming}_NAME)_EXTRA_FRAMEWORKS') or ''
-    module_libraries = variables.get(module_name + '_LIBRARIES') or variables.get(
-        f'$({module_type_naming}_NAME)_LIBRARIES') or ''
+    if module_name:
+        module_archs = variables.get(f'ARCHS')
+        module_files = variables.get(module_name + '_FILES') or variables.get(f'$({module_type_naming}_NAME)_FILES') or ''
+        module_cflags = variables.get(module_name + '_CFLAGS') or variables.get('$({module_type_naming}_NAME)_CFLAGS') or ''
+        module_cxxflags = variables.get(module_name + '_CXXFLAGS') or variables.get('$({module_type_naming}_NAME)_CXXFLAGS') or ''
+        module_cflags = variables.get(f'ADDITIONAL_CFLAGS') or ''
+        module_ldflags = variables.get(module_name + '_LDFLAGS') or variables.get(
+            f'$({module_type_naming}_NAME)_LDFLAGS') or ''
+        module_codesign_flags = variables.get(module_name + '_CODESIGN_FLAGS') or variables.get(
+            f'$({module_type_naming}_NAME)_CODESIGN_FLAGS') or ''
+        module_ipath = variables.get(module_name + '_INSTALL_PATH') or variables.get(
+            f'$({module_type_naming}_NAME)_INSTALL_PATH') or ''
+        module_frameworks = variables.get(module_name + '_FRAMEWORKS') or variables.get(
+            f'$({module_type_naming}_NAME)_FRAMEWORKS') or ''
+        module_pframeworks = variables.get(module_name + '_PRIVATE_FRAMEWORKS') or variables.get(
+            f'$({module_type_naming}_NAME)_PRIVATE_FRAMEWORKS') or ''
+        module_eframeworks = variables.get(module_name + '_EXTRA_FRAMEWORKS') or variables.get(
+            f'$({module_type_naming}_NAME)_EXTRA_FRAMEWORKS') or ''
+        module_libraries = variables.get(module_name + '_LIBRARIES') or variables.get(
+            f'$({module_type_naming}_NAME)_LIBRARIES') or ''
 
-    files = []
-    if module_files:
-        tokens = module_files.split(' ')
-        nextisawildcard = False
-        for i in tokens:
-            if '$(wildcard' in i:
-                nextisawildcard = 1
-                continue
-            if nextisawildcard:
-                # We dont want to stop with these till we hit a ')'
-                # thanks cr4shed ._.
-                nextisawildcard = 0 if ')' in i else 1
-                grab = i.split(')')[0]
-                files.append(grab.replace(')', ''))
-                continue
-            files.append(i)
+        files = []
+        if module_files:
+            tokens = module_files.split(' ')
+            nextisawildcard = False
+            for i in tokens:
+                if '$(wildcard' in i:
+                    nextisawildcard = 1
+                    continue
+                if nextisawildcard:
+                    # We dont want to stop with these till we hit a ')'
+                    # thanks cr4shed ._.
+                    nextisawildcard = 0 if ')' in i else 1
+                    grab = i.split(')')[0]
+                    files.append(grab.replace(')', ''))
+                    continue
+                files.append(i)
 
-    module = {
-        'type': module_type,
-        'files': files
-    }
-    if module_name != '':
-        module['name'] = module_name
-    module['frameworks'] = []
-    if module_frameworks != '':
-        module['frameworks'] += module_frameworks.split(' ')
-    if module_pframeworks != '':
-        module['frameworks'] += module_pframeworks.split(' ')
-    if module_eframeworks != '':
-        module['frameworks'] += module_eframeworks.split(' ')
-    if module_libraries != '':
-        module['libs'] = module_libraries
-    if module_archs != '':
-        module['archs'] = module_archs
-    else:
-        module['archs'] = ['arm64', 'arm64e']
-    if module_cflags != '':
-        module['cflags'] = module_cflags
-    if module_cxxflags:
-        module['cxxflags'] = module_cxxflags
-    if module_ldflags:
-        module['ldflags'] = module_ldflags
-    if stage != []:
-        module['stage'] = stage
-    module['arc'] = arc
-    if not root:
-        return module
-    else:
-        mod_dicts.append(module)
-        project['name'] = module['name']
-        modules.append('.')
+        module = {
+            'type': module_type,
+            'files': files
+        }
+        if module_name != '':
+            module['name'] = module_name
+        module['frameworks'] = []
+        if module_frameworks != '':
+            module['frameworks'] += module_frameworks.split(' ')
+        if module_pframeworks != '':
+            module['frameworks'] += module_pframeworks.split(' ')
+        if module_eframeworks != '':
+            module['frameworks'] += module_eframeworks.split(' ')
+        if module_libraries != '':
+            module['libs'] = module_libraries.split(' ')
+        if module_archs != '':
+            module['archs'] = module_archs
+        else:
+            module['archs'] = ['arm64', 'arm64e']
+        if module_cflags != '':
+            module['cflags'] = module_cflags
+        if module_cxxflags:
+            module['cxxflags'] = module_cxxflags
+        if module_ldflags:
+            module['ldflags'] = module_ldflags
+        if stage != []:
+            module['stage'] = stage
+        module['arc'] = arc
+        if not root:
+            return module
+        else:
+            mod_dicts.append(module)
+            project['name'] = module['name']
+            modules.append('.')
 
     if hassubproj and 'SUBPROJECTS' in variables:
         modules = modules + variables['SUBPROJECTS'].split(' ')
