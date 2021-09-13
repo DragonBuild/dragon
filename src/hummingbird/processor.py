e14a227a3b01e75abdfabd548b4f17bdeb004dc7
KritantaDev <KritantaDevelopment@gmail.com>
2021-09-13 17:25:23 -0400

    changes

--- src/hummingbird/processor.py
+++ src/hummingbird/processor.py
@@ -1,5 +1,29 @@
 from .config import ConfigLoader
 
-class ConfigProcessor:
-    def __init__(self, module):
-        
+
+FILE_TYPE_MAP = {
+    "x": "logos",
+    "m": "objc",
+    "o": "object"
+}
+
+
+class BuildAction:
+    def __init__(self, action, filename, output):
+        self.rule = action['rule']
+        self.filename = filename 
+        self.output = output
+
+
+class BuildFile:
+    def __init__(self, filename):
+        ext = filename.split('.')[-1]
+        self.filename = filename
+        self.filetype = FILE_TYPE_MAP[ext]
+
+
+class ModuleConfigProcessor:
+    def __init__(self, driver, module):
+        self.files = []
+        for filename in module.config['files']:
+            self.files.append(BuildFile(filename))
