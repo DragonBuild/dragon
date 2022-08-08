f3f13c0ece9138aa1831120e7352807fb3aa6737
Lightmann <lightmanns11@gmail.com>
2022-08-08 00:17:20 -0400

    Fix dev install script for macOS and remove unnecessary macOS dep from prerun check

--- development_install.sh
+++ development_install.sh
@@ -6,7 +6,6 @@
 
 python3 -m pip uninstall --yes dragon
 python3 -m pip install .
-DRAGON_DIR=.dragon \
-DRAGON_ROOT_DIR=$HOME/$DRAGON_DIR \
+DRAGON_DIR=.dragon && DRAGON_ROOT_DIR=$HOME/$DRAGON_DIR \
 DRAGON_VERS=$(python3 -c 'from dragon.util import version; print(version())') \
 python3 -m dragon.wizard
