b2104c0fb5f9537b70d63049a29ed98167733671
Lightmann <lightmanns11@gmail.com>
2022-12-28 13:22:19 -0500

    Bump ver

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='dragon',
-      version='1.6.8',
+      version='1.6.9',
       description='A powerful toolkit targeting Apple development, research, and packaging.',
       author='cynder',
       url='https://dragon.krit.me/',
