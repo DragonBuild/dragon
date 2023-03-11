e72ad8f439f524c4bcee6f1c3094fcfa9520477d
vr1s <v@vri.sk>
2023-03-11 02:29:48 -0500

    add `u`/`uninstall` command, make help output sexier

---
+++ src/dragongen/cliutils.py
@@ -0,0 +1,32 @@
+
+import sys
+import ruyaml
+import os
+
+
+if __name__ == "__main__":
+    if 'packid' in sys.argv[1]:
+        if os.path.exists('DragonMake'):
+            with open('DragonMake') as fp:
+                data = ruyaml.safe_load(fp)
+                if 'package' in data:
+                    print(data['package'])
+                    exit(0)
+                if 'Package' in data:
+                    print(data['Package'])
+                    exit(0)
+        if os.path.exists('control'):
+            with open('control') as fp:
+                data = ruyaml.safe_load(fp)
+                if 'Package' in data:
+                    print(data['Package'])
+                    exit(0)
+
+        if os.path.exists('layout/DEBIAN/control'):
+            with open('layout/DEBIAN/control') as fp:
+                data = ruyaml.safe_load(fp)
+                if 'Package' in data:
+                    print(data['Package'])
+                    exit(0)
+
+        exit(1)
