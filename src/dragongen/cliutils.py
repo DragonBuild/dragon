9edbbf61c1fee52868be39da432bd2382c5f4181
Lightmann <lightmanns11@gmail.com>
2024-01-13 12:16:37 -0500

    elif the control check

--- src/dragongen/cliutils.py
+++ src/dragongen/cliutils.py
@@ -24,14 +24,12 @@ if __name__ == "__main__":
                 if 'Package' in data:
                     print(data['Package'])
                     exit(0)
-
-        if os.path.exists('layout/DEBIAN/control'):
+        elif os.path.exists('layout/DEBIAN/control'):
             with open('layout/DEBIAN/control') as fp:
                 data = ruyaml.safe_load(fp)
                 if 'Package' in data:
                     print(data['Package'])
                     exit(0)
-
         exit(1)
 
     if 'needsobjcs' in sys.argv[1]:
